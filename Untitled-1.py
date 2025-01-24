88c55b30eba1ea21c610938f16ccd949346fe7c7
Morgarinka <freeswolf@gmail.com>
2025-01-24 14:49:12 +0300

    Создал класс Order и OrderItem

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,30 @@
-def indent_right(s: str, width: int) -> str:
-    return " " * (max(0, width - len(s))) + s
+from sqlalchemy import create_engine, Column, Integer, String, Text, REAL, ForeignKey, Numeric
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import Session, relationship
+
+Base= declarative_base()
+
+class Order(Base):
+    __tablename__='ordes'
+    
+    id= Column(Integer,primary_key=True)
+    user_id=Column(Integer,ForeignKey('user_id',nullabe=False))
+    status=Column(String,default='pending')
+
+    user=relationship('User',back_populates='orders')
+    items=relationship('Orderitem',back_populates='order')
+
+
+class Orderitem(Base):
+    __tablename__='order_items'
+
+    id=Column(Integer,primary_key=True)
+    order_id=Column(Integer,ForeignKey('orders_id'),nullable=False)
+    product_id=Column(Integer,ForeignKey('products_id'),nullable=False)
+    quantity=Column(Integer,default=1)
+    price=Column(REAL,nullable=False)
+
+    order=relationship('Order',back_populates='items')
+    product=relationship('Product')
+
+User.orders=relationship('Order',order_by=Order.id,back_populates='user')
